60f5a711542e5a7c5b2054d159ad93fed7b08d8b
Tóth Béla <lupa7240@gmail.com>
2025-08-05 21:01:14 +0200

    add crafting and adventure poc

--- bot.py
+++ bot.py
@@ -4,10 +4,14 @@ import shutil
 import json
 import random
 import constants
+import asyncio
+import math
 from functools import wraps
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from discord.ext import commands
+from discord.ui.select import BaseSelect
+from typing import Union
 
 
 load_dotenv()
@@ -49,6 +53,27 @@ def add_to_inventory(user_data_path, item, number):
         inventory[item] = number
     with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
                 json.dump(data, file)
+    
+
+def remove_from_inventory(user_data_path, item, number):
+    data = None
+    with open(constants.CHARACTER_DIR + user_data_path, 'r') as file:
+        data = json.load(file)
+    inventory = data['inventory']
+    if item in inventory:
+        inventory[item] = inventory[item] - number
+        if inventory[item] <= 0:
+            del inventory[item]
+    with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
+                json.dump(data, file)
+
+
+def fetch_user_inventory(user_data_path):
+    data = None
+    with open(constants.CHARACTER_DIR + user_data_path, 'r') as file:
+        data = json.load(file)
+    return data['inventory']
+
 
 def remove_current_activity(user_data_path):
     data = None
@@ -56,10 +81,37 @@ def remove_current_activity(user_data_path):
         data = json.load(file)
     del data[constants.JSON_ACTIVITY]
     with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
-                json.dump(data, file)
+        json.dump(data, file)
 
+def set_activity(activity, end_time, duration, data, user_data_path, subtype = None):
+    activity = {"activity":activity,"end_time":end_time.isoformat(),"duration":duration,"subtype":subtype}
+    data[constants.JSON_ACTIVITY] = activity
+    with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
+        json.dump(data, file)
 
-def resolve_activity(activity, duration, user_data, user_data_path):
+def fetch_crafting_recipies_for_activity(activity: str):
+    table = None
+    table_path = f"{constants.CRAFTING_RECIPE_DIR}/{activity}.json"
+    with open(table_path, 'r') as loot_table_file:
+        table = json.load(loot_table_file)
+    return table
+
+def init_new_skill(user_data_path, activity):
+    data = None
+    with open(constants.CHARACTER_DIR + user_data_path, 'r') as file:
+        data = json.load(file)
+    data[constants.JSON_SKILLS][activity] = {
+        "current_level": 0,
+        "current_xp": 0,
+    }
+    with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
+        json.dump(data, file)
+
+def resolve_activity(activity, duration, user_data: dict, user_data_path):
+    #check if user has the skill or not (handle new skills)
+    if not activity in user_data[constants.JSON_SKILLS]:
+        init_new_skill(user_data_path, activity)
+        user_data = fetch_user_data(user_data_path)
     activity_level = user_data[constants.JSON_SKILLS][activity]['current_level']
     loot_text = "Caught: "
     looted = dict()
@@ -76,6 +128,24 @@ def resolve_activity(activity, duration, user_data, user_data_path):
                 drop_name = fetch_weighted_loot(drops)
                 drop_amount = fetch_loot_ammount_by_weights(loot_items=loot_table, rarity=drop_rarity, loot=drop_name)
                 looted[drop_name] = looted.get(drop_name, 0) + drop_amount
+            case constants.ACTIVITY_SMITHING | constants.ACTIVITY_COOKING:
+                recipies = fetch_crafting_recipies_for_activity(activity)
+                #fetch currently processed recipe
+                current_rec = user_data[constants.JSON_ACTIVITY][constants.JSON_CRAFTING_RECIPE]
+                # figure out which materials we have less to process
+                inventory = fetch_user_inventory(user_data_path=user_data_path)
+                current_ingredients = recipies.get(current_rec).get('cost')
+                if can_craft(current_ingredients, inventory):
+                    for item, cost in current_ingredients.items():
+                        if item in inventory:
+                            remove_from_inventory(user_data_path, item, cost)
+                            inventory[item] = inventory[item] - cost
+                            if inventory[item] <= 0:
+                                del inventory[item]
+                    crafted_items = recipies.get(current_rec).get('amount')
+                    looted[current_rec] = looted.get(current_rec, 0) + crafted_items
+                else:
+                    break
             case _:
                 print("non valid activity")
 
@@ -119,6 +189,15 @@ def fetch_loot_ammount_by_weights(loot_items: dict, rarity: str, loot: str) -> i
     roll_high = roll_low = loot_items[rarity][loot]['range']['high']
     return random.randint(roll_low, roll_high)
 
+def can_craft(ingredients, inventory):
+    for item, cost in ingredients.items():
+        if item in inventory:
+            if (inventory[item] < cost) and (inventory[item] >= 0):
+                return False
+        else:
+            return False
+    return True
+
 ############
 # DECORATORS
 ############
@@ -156,6 +235,54 @@ def required_idle_character():
                 return await func(ctx, *args, **kwargs)
         return wrapper
     return decorator
+
+############
+# CLASSES
+############
+
+# Defines a custom Select containing colour options
+# that the user can choose. The callback function
+# of this class is called when the user changes their choice
+class Dropdown(discord.ui.Select):
+    def __init__(self, options_sent, craft_type, duration):
+
+        # Set the options that will be presented inside the dropdown
+        options = []
+        for opt in options_sent:
+            options.append(discord.SelectOption(label=opt['name'],emoji="🛠️",description=opt['desc']))
+
+        # The placeholder is what will be shown when no option is chosen
+        # The min and max values indicate we can only pick one of the three options
+        # The options parameter defines the dropdown options. We defined this above
+        self.craft_type = craft_type
+        self.duration = duration
+        super().__init__(placeholder='Choose what to craft ...', min_values=1, max_values=1, options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        # Use the interaction object to send a response message containing
+        # the user's favourite colour or choice. The self object refers to the
+        # Select object, and the values attribute gets a list of the user's
+        # selected options. We only want the first one.
+        self.disabled = True
+        await interaction.response.send_message(f'You selected {self.values[0]}')
+        user_name = interaction.user.name
+        user_id = interaction.user.id
+        user_data_path = fetch_user_data_path(user_name, user_id)
+        data = fetch_user_data(user_data_path)
+        start_time = datetime.now()
+        end_time = start_time + timedelta(minutes=self.duration)
+        value_to_json = self.values[0].lower().replace(" ","_")
+        set_activity(self.craft_type,end_time,self.duration,data,user_data_path, value_to_json)
+
+        await interaction.message.edit(view=self.view)
+
+
+class DropdownView(discord.ui.View):
+    def __init__(self, options_sent, craft_type, duration):
+        super().__init__()
+        # Adds the dropdown to our view object.
+        self.add_item(Dropdown(options_sent, craft_type, duration))
+
 
 ############
 # COMMANDS
@@ -222,11 +349,9 @@ async def fishing_command(ctx, arg1: int = commands.parameter(default=1, descrip
     data = fetch_user_data(user_data_path)
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=arg1)
-    activity = {"activity":"fishing","end_time":end_time.isoformat(),"duration":arg1}
-    data[constants.JSON_ACTIVITY] = activity
-    with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
-        json.dump(data, file)
+    set_activity('fishing',end_time,arg1,data,user_data_path)
     await ctx.send(f"Went fishing hopefully, until: {end_time.isoformat()}")
+
 
 @bot.command(name='mining')
 @requires_character_registered()
@@ -236,11 +361,98 @@ async def mining_command(ctx, arg1: int = commands.parameter(default=1, descript
     data = fetch_user_data(user_data_path)
     start_time = datetime.now()
     end_time = start_time + timedelta(minutes=arg1)
-    activity = {"activity":"mining","end_time":end_time.isoformat(),"duration":arg1}
-    data[constants.JSON_ACTIVITY] = activity
-    with open(constants.CHARACTER_DIR + user_data_path, "w") as file:
-        json.dump(data, file)
+    set_activity('mining',end_time,arg1,data,user_data_path)
     await ctx.send(f"Went mining hopefully, until: {end_time.isoformat()}")
+
+
+@bot.command(name='adventure')
+@requires_character_registered()
+@required_idle_character()
+async def adventure_command(ctx, arg1: int = commands.parameter(default=1, description="Minutes to spend on adventuring")):
+    adventure_text = """Choose where to spend your time:
+    🌳 - Forest
+    ⛰️ - Mountains
+    ⛏️ - Mines
+    🏜️ - Desert
+    """
+    message = await ctx.send(adventure_text)
+    options = ("🌳","⛰️","⛏️","🏜️")
+    for opt in options:
+        await message.add_reaction(opt)
+
+    def check(r: discord.Reaction, u: Union[discord.Member, discord.User]):  # r = discord.Reaction, u = discord.Member or discord.User.
+        return u.id == ctx.author.id and r.message.channel.id == ctx.channel.id and \
+               str(r.emoji) in options
+        # checking author, channel and only having the check become True when detecting a ✅ or ❌
+        # else, it will timeout.
+
+    try:
+        #                         event = on_reaction_add without on_
+        reaction, user = await bot.wait_for('reaction_add', check = check, timeout = 60.0)
+        # reaction = discord.Reaction, user = discord.Member or discord.User.
+    except asyncio.TimeoutError:
+        # at this point, the check didn't become True.
+        await ctx.send(f"**{ctx.author}**, you didnt react in 60 seconds.")
+        return
+    else:
+        # at this point, the check has become True and the wait_for has done its work, now we can do ours.
+        # here we are sending some text based on the reaction we detected.
+        
+        for emoji in options:
+            if str(reaction.emoji) == emoji:
+                 return await ctx.send(f"{ctx.author} adventured into {emoji} for {arg1} minutes.")
+
+@bot.command(name='smithing')
+@requires_character_registered()
+@required_idle_character()
+async def smithing_options(ctx, arg1: int = commands.parameter(default=1, description="Minutes to spend on adventuring")):
+    all_options = {
+        "iron_ingot" : {"name":"Iron Ingot", "desc":"Craft 1 Iron Ingot with 2 coal and 1 Iron Ore"},
+        "iron_plate" : {"name":"Iron Plate", "desc":"Craft 1 Iron Plate with 2 coal and 1 Iron Ingot"}
+    }
+
+    craft_options = []
+    recipies = fetch_crafting_recipies_for_activity(constants.ACTIVITY_SMITHING)
+    #fetch currently processed recipe
+    user_data_path = fetch_user_data_path(ctx.author.name, ctx.author.id)
+    inventory = fetch_user_inventory(user_data_path=user_data_path)
+    for current_rec in recipies:
+        current_ingredients = recipies.get(current_rec).get('cost')
+        if can_craft(current_ingredients, inventory):
+            craft_options.append(all_options[current_rec])
+
+    view = DropdownView(craft_options, 'smithing', arg1)
+    if len(craft_options) > 0:
+    # Sending a message containing our view
+        await ctx.send('What are we smithing:', view=view)
+    else:
+        await ctx.send(constants.NO_INGREDIENTS_FOR_CRAFT)
+
+@bot.command(name='cooking')
+@requires_character_registered()
+@required_idle_character()
+async def cooking_options(ctx, arg1: int = commands.parameter(default=1, description="Minutes to spend on adventuring")):
+    all_options = {
+        "cooked_fish" : {"name":"Cooked Fish", "desc":"Cook 1 Cooked Fish with 1 fish and 1 coal"},
+        "apple_pie" : {"name":"Apple Pie", "desc":"Craft 1 Iron Plate with 2 coal and 1 Iron Ingot"}
+    }
+
+    craft_options = []
+    recipies = fetch_crafting_recipies_for_activity(constants.ACTIVITY_COOKING)
+    #fetch currently processed recipe
+    user_data_path = fetch_user_data_path(ctx.author.name, ctx.author.id)
+    inventory = fetch_user_inventory(user_data_path=user_data_path)
+    for current_rec in recipies:
+        current_ingredients = recipies.get(current_rec).get('cost')
+        if can_craft(current_ingredients, inventory):
+            craft_options.append(all_options[current_rec])
+
+    view = DropdownView(craft_options, constants.ACTIVITY_COOKING, arg1)
+    if len(craft_options) > 0:
+    # Sending a message containing our view
+        await ctx.send('What are we cooking:', view=view)
+    else:
+        await ctx.send(constants.NO_INGREDIENTS_FOR_CRAFT)
 
 @bot.command(name='inventory')
 @requires_character_registered()
--- constants.py
+++ constants.py
@@ -6,17 +6,22 @@ CHARACTER_OCCUPIED = "Character already occupied"
 CHARACTER_IDLE = "Character is idle"
 CHARACTER_FINISHED = "Character finished"
 INVENTORY_EMPTY = "Inventory empty"
+NO_INGREDIENTS_FOR_CRAFT = "Not enough ingredients to do this."
 
 JSON_ACTIVITY = "current_activity"
 JSON_ACTIVITY_DURATION = "duration"
 JSON_INVENTORY = "inventory"
 JSON_STATS = "stats"
 JSON_SKILLS = "skills"
-
+JSON_CRAFTING_RECIPE = "subtype"
 
 ACTIVITY_FISHING = "fishing"
 ACTIVITY_MINING = "mining"
+ACTIVITY_SMITHING = "smithing"
+ACTIVITY_COOKING = "cooking"
 
 LOOT_TABLE_DIR = "./loot_tables/"
+CRAFTING_RECIPE_DIR = "./crafting/"
+
 
 DEFAULT_RARITY_CHANCES = [70, 20, 9, 1]
