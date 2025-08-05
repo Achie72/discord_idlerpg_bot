cea19fc822bd110b34bf2021f225bc725f7e68c5
Tóth Béla <lupa7240@gmail.com>
2025-08-05 13:04:27 +0200

    Add loot tables for activities

---
+++ constants.py
@@ -0,0 +1,22 @@
+CHARACTER_ALREADY_CREATED = "Character already created"
+CHARACTER_CREATED = "Character created"
+CHARACTER_NOT_CREATED = "Character not created yet. use init command."
+CHARACTER_DIR = "./character_data/"
+CHARACTER_OCCUPIED = "Character already occupied"
+CHARACTER_IDLE = "Character is idle"
+CHARACTER_FINISHED = "Character finished"
+INVENTORY_EMPTY = "Inventory empty"
+
+JSON_ACTIVITY = "current_activity"
+JSON_ACTIVITY_DURATION = "duration"
+JSON_INVENTORY = "inventory"
+JSON_STATS = "stats"
+JSON_SKILLS = "skills"
+
+
+ACTIVITY_FISHING = "fishing"
+ACTIVITY_MINING = "mining"
+
+LOOT_TABLE_DIR = "./loot_tables/"
+
+DEFAULT_RARITY_CHANCES = [70, 20, 9, 1]
